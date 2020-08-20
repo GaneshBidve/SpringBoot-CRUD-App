3ecacb9f849b88ea55d2279b6c5fc1f5a70bdd88
Ganesh_Bidve <ganeshbidve07@gmail.com>
2020-08-20 18:05:37 +0530

    created controller and service classes

---
+++ src/main/java/com/csi/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.csi.service;
+
+import java.util.List;
+
+import com.csi.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+}
---
+++ src/main/java/com/csi/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.csi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csi.dao.EmployeeRepository;
+import com.csi.model.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+}
