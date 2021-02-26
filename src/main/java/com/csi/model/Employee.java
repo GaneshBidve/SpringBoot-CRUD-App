d43077c5b9cd1b30a4fc1105123fab9047191196
Ganesh V Bidve <31819293+GaneshBidve@users.noreply.github.com>
2021-02-26 15:09:12 +0530

    updated lombok

--- src/main/java/com/csi/model/Employee.java
+++ src/main/java/com/csi/model/Employee.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table(name = "empl")
 public class Employee {
@@ -23,37 +24,4 @@ public class Employee {
 
 	@Column(name = "email")
 	private String email;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 }
